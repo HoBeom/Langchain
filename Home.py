6505643ce8b813fd4a675a0db9bb7900f0eb87a6
HoBeom <jhb1365@gmail.com>
2025-01-16 00:30:29 +0900

    Day 09: Streamlit, Document GPT

---
+++ Home.py
@@ -0,0 +1,14 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title="LangChain Streamlit Dashboard",
+    page_icon="🤖",
+)
+
+st.markdown(
+    """
+    ## LangChain Streamlit Dashboard
+
+- [ ] [DocumentGPT](/DocumentGPT)
+"""
+)
