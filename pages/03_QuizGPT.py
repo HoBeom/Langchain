043d1a59174221be762b9a3efc137e3dcb81531f
HoBeom <jhb1365@gmail.com>
2025-01-21 01:05:37 +0900

    UPDATE QuizGPT.py function call

--- pages/03_QuizGPT.py
+++ pages/03_QuizGPT.py
@@ -2,41 +2,50 @@ import json
 from langchain.document_loaders import UnstructuredFileLoader
 from langchain.text_splitter import CharacterTextSplitter
 from langchain.chat_models import ChatOpenAI
-from langchain.prompts import ChatPromptTemplate
+from langchain.prompts import PromptTemplate
 from langchain.callbacks import StreamingStdOutCallbackHandler
 import streamlit as st
 import regex as re
 from langchain.retrievers import WikipediaRetriever
-from langchain.schema import BaseOutputParser, output_parser
 from langchain.schema import HumanMessage
 
-class JsonOutputParser(BaseOutputParser):
-    def parse(self, text):
-        result = None
-        text = text.replace("```", "").replace("json", "")
-        try:
-            result = json.loads(text)
-        except json.JSONDecodeError as e:
-            raise ValueError(f"Error decoding JSON: {e}")
-        except Exception as e:
-            raise ValueError(f"Unexpected error: {e}")
-        if result is not None:
-            return result
-        try:
-            json_pattern = r"\{(?:[^{}]|(?R))*\}"
-            match = re.search(json_pattern, text, re.DOTALL)
-            if not match:
-                raise ValueError("No valid JSON found in the input text.")
-            json_str = match.group(0)
-            parsed_json = json.loads(json_str)
-            return parsed_json
-        except json.JSONDecodeError as e:
-            raise ValueError(f"Error decoding JSON: {e}")
-        except Exception as e:
-            raise ValueError(f"Unexpected error: {e}")
-
-
-output_parser = JsonOutputParser()
+function = {
+    "name": "create_quiz",
+    "description": "function that takes a list of questions and answers and returns a quiz",
+    "parameters": {
+        "type": "object",
+        "properties": {
+            "questions": {
+                "type": "array",
+                "items": {
+                    "type": "object",
+                    "properties": {
+                        "question": {
+                            "type": "string",
+                        },
+                        "answers": {
+                            "type": "array",
+                            "items": {
+                                "type": "object",
+                                "properties": {
+                                    "answer": {
+                                        "type": "string",
+                                    },
+                                    "correct": {
+                                        "type": "boolean",
+                                    },
+                                },
+                                "required": ["answer", "correct"],
+                            },
+                        },
+                    },
+                    "required": ["question", "answers"],
+                },
+            }
+        },
+        "required": ["questions"],
+    },
+}
 
 st.set_page_config(
     page_title="QuizGPT",
@@ -134,201 +143,51 @@ if not st.session_state["key_test"]:
     st.error("⚠️ Please enter a valid OpenAI API Key to proceed.")
     st.stop()
 
-llm = ChatOpenAI(
-    temperature=0.1,
-    model=model_select,
-    openai_api_key=key,
-    streaming=True,
-    callbacks=[StreamingStdOutCallbackHandler()],
-)
-
 def format_docs(docs):
     return "\n\n".join(document.page_content for document in docs)
 
-questions_prompt = ChatPromptTemplate.from_messages(
-    [
-        (
-            "system",
-            """
+prompt = PromptTemplate.from_template(
+    """
     You are a helpful assistant that is role playing as a teacher.
-         
-    Based ONLY on the following context make 5 (FIVE) questions to test the user's knowledge about the text.
+                    
+    Based ONLY on the following context make 5 questions to test the user's knowledge about the text.
     
     Each question should have 4 answers, three of them must be incorrect and one should be correct.
-         
+
     The difficulty level of the problem is '{level}'.
-         
-    Use (o) to signal the correct answer.
-         
-    Question examples:
-         
-    Question: What is the color of the ocean?
-    Answers: Red|Yellow|Green|Blue(o)
-         
-    Question: What is the capital or Georgia?
-    Answers: Baku|Tbilisi(o)|Manila|Beirut
-         
-    Question: When was Avatar released?
-    Answers: 2007|2001|2009(o)|1998
-         
-    Question: Who was Julius Caesar?
-    Answers: A Roman Emperor(o)|Painter|Actor|Model
-         
-    Your turn!
-         
+
     Context: {context}
-""",
-        )
-    ]
+"""
 )
-
-# questions_chain = {"context": format_docs } | questions_prompt | llm
-questions_chain = questions_prompt | llm
-
-formatting_prompt = ChatPromptTemplate.from_messages(
-    [
-        (
-            "system",
-            """
-    You are a powerful formatting algorithm.
-     
-    You format exam questions into JSON format.
-    Answers with (o) are the correct ones.
-     
-    Example Input:
-
-    Question: What is the color of the ocean?
-    Answers: Red|Yellow|Green|Blue(o)
-         
-    Question: What is the capital or Georgia?
-    Answers: Baku|Tbilisi(o)|Manila|Beirut
-         
-    Question: When was Avatar released?
-    Answers: 2007|2001|2009(o)|1998
-         
-    Question: Who was Julius Caesar?
-    Answers: A Roman Emperor(o)|Painter|Actor|Model
-    
-     
-    Example Output:
-     
-    ```json
-    {{ "questions": [
-            {{
-                "question": "What is the color of the ocean?",
-                "answers": [
-                        {{
-                            "answer": "Red",
-                            "correct": false
-                        }},
-                        {{
-                            "answer": "Yellow",
-                            "correct": false
-                        }},
-                        {{
-                            "answer": "Green",
-                            "correct": false
-                        }},
-                        {{
-                            "answer": "Blue",
-                            "correct": true
-                        }}
-                ]
-            }},
-                        {{
-                "question": "What is the capital or Georgia?",
-                "answers": [
-                        {{
-                            "answer": "Baku",
-                            "correct": false
-                        }},
-                        {{
-                            "answer": "Tbilisi",
-                            "correct": true
-                        }},
-                        {{
-                            "answer": "Manila",
-                            "correct": false
-                        }},
-                        {{
-                            "answer": "Beirut",
-                            "correct": false
-                        }}
-                ]
-            }},
-                        {{
-                "question": "When was Avatar released?",
-                "answers": [
-                        {{
-                            "answer": "2007",
-                            "correct": false
-                        }},
-                        {{
-                            "answer": "2001",
-                            "correct": false
-                        }},
-                        {{
-                            "answer": "2009",
-                            "correct": true
-                        }},
-                        {{
-                            "answer": "1998",
-                            "correct": false
-                        }}
-                ]
-            }},
-            {{
-                "question": "Who was Julius Caesar?",
-                "answers": [
-                        {{
-                            "answer": "A Roman Emperor",
-                            "correct": true
-                        }},
-                        {{
-                            "answer": "Painter",
-                            "correct": false
-                        }},
-                        {{
-                            "answer": "Actor",
-                            "correct": false
-                        }},
-                        {{
-                            "answer": "Model",
-                            "correct": false
-                        }}
-                ]
-            }}
-        ]
-     }}
-    ```
-    Your turn!
-
-    Questions: {context}
-
-""",
-        )
-    ]
-)
-
-formatting_chain = formatting_prompt | llm
 
 @st.cache_data(show_spinner="Making quiz...")
-def generate_questions(_docs, level):
-    return questions_chain.invoke({"context": format_docs(_docs), "level": level}).content
-
-@st.cache_data(show_spinner="Formatting quiz...")
-def format_questions(qize):
-    chain = formatting_chain | output_parser
-    return chain.invoke({"context": qize})
+def run_quiz_chain(_docs, topic, level):
+    chain = prompt | llm
+    return chain.invoke({"context": _docs, "level": level})
 
 if not docs:
     st.info("🔑 **API Key Verified!** You are now connected to OpenAI's API. Ready to generate quizzes.")
     st.info("🔍 **Choose Your Source:** Upload a file or search Wikipedia in the sidebar to begin.")
 else:
-    response = format_questions(generate_questions(docs, level=level))
+    llm = ChatOpenAI(
+        temperature=0.1,
+        streaming=True,
+        callbacks=[
+            StreamingStdOutCallbackHandler(),
+        ],
+        openai_api_key=key,
+    ).bind(
+        function_call={
+            "name": "create_quiz",
+        },
+        functions=[
+            function,
+        ],
+    )
+    response = run_quiz_chain(docs, topic if topic else file.name, level)
+    response = response.additional_kwargs["function_call"]["arguments"]
     with st.form("questions_form"):
-        # st.write(response)
-        questions = response["questions"]
+        questions = json.loads(response)["questions"]
         question_count = len(questions)
         success_count = 0
         for question in questions:
